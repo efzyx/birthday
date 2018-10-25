4e9834a9d5e713a05d2289696cf85cd474595abb
efzet <fauzi.padlaw@gmail.com>
2018-10-25 22:41:51 +0700

    ganti story

--- scripts/effect.js
+++ scripts/effect.js
@@ -252,8 +252,8 @@ $('document').ready(function(){
 			$("p:nth-child("+i+")").fadeOut('slow').delay(800).promise().done(function(){
 			i=i+1;
 			$("p:nth-child("+i+")").fadeIn('slow').delay(1000);
-			if(i==50){
-				$("p:nth-child(49)").fadeOut('slow').promise().done(function () {
+			if(i==54){
+				$("p:nth-child(53)").fadeOut('slow').promise().done(function () {
 					$('.cake').fadeIn('fast');
 				});
 				
